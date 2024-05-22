f40c844ef4046bc7348f732e5edbdc2b6f95581d
Ichedialbright <130225531+Ichedialbright@users.noreply.github.com>
2024-05-22 09:05:35 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,14 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+
+public class Main{
+
     public static void main(String[] args) {
-        Student Albright = new Student();
-        Albright.registerCourses("java");
-        System.out.println(Albright.checkResult());
+        CMP202 cmp202 = new CMP202();
+        //cmp202.printUserDetails();
 
+        Person person = new Person(); // Constructor(first method that gets called when you create an instance of a class
+        person.setName("Albright");
+//        System.out.println(person.getName());
 
+        person.setPassword("123456");
+//        System.out.println(person.getPassword());
 
+        Physics physics = new Physics();
+        System.out.println(physics.calculateVelocity(8, 4));
+        System.out.println(physics.calculateVelocity(2,9,6));
     }
-
-
 }
